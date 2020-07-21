222cc07785a9996b995ce0c935d3fbd1d93d86d9
thoughtful-cookie-2669 <scherezade.momin@gmail.com>
2020-07-21 20:02:39 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,7 +1,6 @@
 # Use this space to code along with the readme
 
 def say_hello_world_ten_times
-
   phrase = "Hello World!"
   puts phrase
   puts phrase
@@ -13,3 +12,6 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
   puts phrase
+end
+
+say_hello_world_ten_times
